a771d9fd841919feb61d87a7807df52540272829
Blarz hdz <info.redpaz@gmail.com>
2018-03-24 23:57:49 -0600

    counter

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,6 +1,38 @@
 <?php
+
+  $fileName = 'visitors.txt';
+  $filePath = dirname(__FILE__).'/'.$fileName;
+
+  if (is_file($filePath)) {
+
+    $fp = fopen($filePath, "c+");            
+    $data = fread($fp, filesize($filePath)); 
+    $arr = explode("\n", $data);             
+
+    $c=0;
+    foreach($arr as $visit) {
+      $c++;
+    }
+    fwrite($fp,date('Y-m-d H:i:s')."\n");
+
+
+    fclose($fp);
+
+  } else {
+   
+    $fp = fopen($filePath, "w"); 
+    $c=1;
+    fwrite($fp, date('Y-m-d H:i:s')."\n");    
+    fclose($fp);
+
+  }
+
+?>
+<?php
 include(APPPATH.'config/constants.php');
 ?>
+<hr>
+<button type="button" class="btn btn-primary btn-lg btn-block">Visitors: <?php echo $c;?></button>
 <nav class="custom">
   <a class="nav-link active" href="#">Version App -><?php echo APP_VERSION;?></a>
   
